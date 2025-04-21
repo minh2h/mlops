8f8b2c688c4eecdbf7092f5f1dde4a5eab09486a
minh2h <minhtethein001@gmail.com>
2025-04-21 22:13:32 +0800

    initial addition to the flask customer churn model

---
+++ churn_prediction/train.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+
+from sklearn.metrics import roc_curve
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_auc_score
+
+df = pd.read_csv('WA_Fn-UseC_-Telco-Customer-Churn.csv')
+df.columns = df.columns.str.lower().str.replace(' ', '_')
+
+categorical_columns = list(df.dtypes[df.dtypes == 'object'].index)
+
+for c in categorical_columns:
+    df[c] = df[c].str.lower().str.replace(' ', '_')
+
+df.totalcharges = pd.to_numeric(df.totalcharges, errors='coerce')
+df.totalcharges = df.totalcharges.fillna(0)
+
+df.churn = (df.churn == 'yes').astype(int)
